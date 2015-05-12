72d8aa618b3bb1aa610682ad9167f6b8639450fa
Knut Kiesel <kiesel@cern.ch>
2015-05-12 14:32:20 +0200

    corrected typo in example srm path (sie -> size)

--- mergeTier2Files.py
+++ mergeTier2Files.py
@@ -6,7 +6,7 @@ usage ./mergeTier2Files.py <outputFile>.root <srm-source-path>
   "srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/<...>/<date>_<time>/"
   the "<date>_<time>" directory contains numbered directries "0001","0002",... that contain the root files
 example usage:
-./mergeTier2Files.py out.root srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/jolange/data/Test/GJets_HT-400to600_Tune4C_13TeV-madgraph-tauola/sieCheck/150504_120017/
+./mergeTier2Files.py out.root srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms/store/user/jolange/data/Test/GJets_HT-400to600_Tune4C_13TeV-madgraph-tauola/sizeCheck/150504_120017/
 """
 import subprocess as sp
 import sys
