b51e14810ea0bbc1f61603cf9ff80f7cda9306ad
Johannes Lange <johannes.lange@rwth-aachen.de>
2015-07-07 16:54:09 +0200

    also ignoring log folders

--- mergeTier2Files.py
+++ mergeTier2Files.py
@@ -38,8 +38,10 @@ def getDirectoryContent(srmDirectoryPath):
     # only every second entry is name, the rest are sizes
     # and the very first entry is the directory name
     contents=contents[3::2]
-    # ignore the files placed in the subfolder 'failed' and the folder itself
-    contents=[f for f in contents if not f.endswith("/failed/")]
+    # ignore the files placed in the subfolders not containing the
+    # relevant data files and the folders themselves
+    ignores=("/failed/","/log/")
+    contents=[f for f in contents if not f.endswith(ignores)]
     return contents
 
 def getFilePaths(srmDirectoryPath):
