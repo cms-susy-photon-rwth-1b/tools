5f55b5b9d2a167c7b040a25df5e1e3e981a0607a
Knut Kiesel <kiesel@cern.ch>
2015-08-25 14:57:55 +0200

    adding command line options; string searching and formating with re-module

--- listMergeCommands.py
+++ listMergeCommands.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 
 import subprocess
+import argparse
+import re
 
 
 def modifyDatasetName( dataset ):
     # shorten dataset name
-    deletes = [ "_13TeV", "-madgraph", "_Tune4C", "-tauola" ]
+    deletes = [ "_13TeV", "-madgraph", "_Tune4C", "-tauola", "_MSDecaysCKM_central", "_TuneCUETP8M1", "-amcatnloFXFX", "-madspin", "-pythia8" ]
     for d in deletes:
         dataset = dataset.replace( d, "" )
     return dataset
 
 def main():
-    srmPrefix = "srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN="
-    user = "jolange"
-    outputPath = "/user/kiesel/nTuples"
+    parser = argparse.ArgumentParser(description="List posslible merge commands based on the available files.")
+    parser.add_argument("--userInputPath", default="kiesel/13TeV/nTuples")
+    parser.add_argument("--outputPath", default="/user/kiesel/nTuples")
+    parser.add_argument("--srmPrefix", default="srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=")
+    args = parser.parse_args()
 
-    srmlsOut = subprocess.check_output( "srmls -recursion_depth=5 {}/pnfs/physik.rwth-aachen.de/cms/store/user/{}/".format(srmPrefix,user), shell=True )
+    srmlsOut = subprocess.check_output( "srmls -recursion_depth=5 {}/pnfs/physik.rwth-aachen.de/cms/store/user/{}".format(args.srmPrefix,args.userInputPath), shell=True )
     for line in srmlsOut.split("\n"):
-        if "                   " in line:
-
-            inFolder = line.split(" ")[-1]
-            dataset = inFolder.split("/")[9]
+        match = re.match( ".*/([^/]+)/.*/\d\d\d\d\d\d_\d\d\d\d\d\d/$", line )
+        if match:
+            dataset = match.groups()[0]
+            inFolder = line.replace("512", "").strip()
             dataset = modifyDatasetName( dataset )
-            print "./mergeTier2Files.py %s/%s_nTuple.root %s%s"%(outputPath,dataset,srmPrefix,inFolder)
+            print "./mergeTier2Files.py %s/%s_nTuple.root %s%s"%(args.outputPath,dataset,args.srmPrefix,inFolder)
 
 if __name__ == "__main__":
     main()
