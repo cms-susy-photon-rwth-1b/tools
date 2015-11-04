c3b73af0a59e2b9ebf1903018ec64589bd7127e0
Johannes Lange <johannes.lange@rwth-aachen.de>
2015-11-04 12:07:21 +0100

    added joFormat option

--- mergeCommandFromCrabDir.py
+++ mergeCommandFromCrabDir.py
@@ -32,12 +32,23 @@ def main():
     parser = argparse.ArgumentParser(description="List merge command based on crab directory.")
     parser.add_argument("crabDir")
     parser.add_argument( "-o", "--out", default="test.root")
+    parser.add_argument('--joFormat',dest="joFormat", action='store_const',const=True,default=False,
+                        help="Johannes' output format")
+
     args = parser.parse_args()
 
     infos = getInfoFromDir( args.crabDir )
-    infos["out"] = args.out
+    if not args.joFormat:
+        infos["out"] = args.out
+        infos["mergeCommand"]="./mergeTier2Files.py"
+    else:
+        from listMergeCommands import modifyDatasetName
+        outFileName=args.crabDir[:-1]
+        outFileName=modifyDatasetName(outFileName.replace("crab_",""))
+        infos["out"] = "/user/lange/data/run2/dl/"+outFileName+".root"
+        infos["mergeCommand"]="mergeTier2Files.py"
 
-    print "./mergeTier2Files.py {out} srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms{lfn}/{dset}/{publishDataName}/{time}/".format( **infos )
+    print "{mergeCommand} {out} srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2?SFN=/pnfs/physik.rwth-aachen.de/cms{lfn}/{dset}/{publishDataName}/{time}/".format( **infos )
 
 if __name__ == "__main__":
     main()
