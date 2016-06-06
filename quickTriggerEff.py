8d7dbb32a926c8ff7428211f3869c3136ee1ec48
Johannes Lange <johannes.lange@rwth-aachen.de>
2016-06-06 16:55:37 +0200

    efficiency is plotted vs. run number

--- quickTriggerEff.py
+++ quickTriggerEff.py
@@ -6,6 +6,7 @@ import ROOT as rt
 rt.gErrorIgnoreLevel=rt.kError
 import sys
 from array import array
+from collections import defaultdict
 
 def readTree( filename, treename ):
     """
@@ -18,32 +19,72 @@ def readTree( filename, treename ):
     return tree
 
 def trigger_eff(filename, treename):
-   tree = readTree(filename,treename)
+    tree = readTree(filename,treename)
 
-   # eff=rt.TEfficiency("",";pt;eff",100,0,800)
-   bins=range(0,160,20)
-   bins+=range(160,200,10)
-   bins+=range(200,900,50)
-   print bins
+    # eff=rt.TEfficiency("",";pt;eff",100,0,800)
+    bins=range(0,160,20)
+    bins+=range(160,200,10)
+    bins+=range(200,900,50)
+    print bins
 
-   eff=rt.TEfficiency("",";pt;eff",len(bins)-1,array('d',bins))
-   for evt in tree:
-      pt=-1
-      for ph in evt.photons:
-         if not ph.hasPixelSeed and abs(ph.p.Eta())<1.4442:
-            pt=ph.p.Pt()
-            break
-      if pt<0: continue
+    runRange=(273100,273800)
 
-      baseTr=evt.HLT_PFHT125_v or evt.HLT_PFHT200_v or evt.HLT_PFHT250_v or evt.HLT_PFHT300_v or evt.HLT_PFHT350_v or evt.HLT_PFHT400_v or evt.HLT_PFHT475_v or evt.HLT_PFHT600_v or evt.HLT_PFHT650_v or evt.HLT_PFHT800_v
-      # baseTr=evt.HLT_PFMET170_NoiseCleaned_v or evt.HLT_PFMET170_HBHECleaned_v or evt.HLT_PFMET170_JetIdCleaned_v or evt.HLT_PFMET170_NotCleaned_v
-      sigTr=evt.HLT_Photon165_HE10_v
+    eff=rt.TEfficiency("",";pt;eff",len(bins)-1,array('d',bins))
+    mRunTotal=defaultdict(int)
+    mRunPass=defaultdict(int)
+    for evt in tree:
+        pt=-1
+        for ph in evt.photons:
+            if not ph.hasPixelSeed and abs(ph.p.Eta())<1.4442:
+                pt=ph.p.Pt()
+                break
+        if pt<0: continue
 
-      if baseTr:
-         eff.Fill(sigTr,pt)
-   eff.Draw("AP")
-   print "pt>%.0f: %.1f+%.1f-%.1f"%(getPlateauEfficiency(eff,180))
-   raw_input("...")
+        baseTr=evt.HLT_PFHT125_v or evt.HLT_PFHT200_v or evt.HLT_PFHT250_v or evt.HLT_PFHT300_v or evt.HLT_PFHT350_v or evt.HLT_PFHT400_v or evt.HLT_PFHT475_v or evt.HLT_PFHT600_v or evt.HLT_PFHT650_v or evt.HLT_PFHT800_v
+        # baseTr=evt.HLT_PFMET170_NoiseCleaned_v or evt.HLT_PFMET170_HBHECleaned_v or evt.HLT_PFMET170_JetIdCleaned_v or evt.HLT_PFMET170_NotCleaned_v
+        sigTr=evt.HLT_Photon165_HE10_v
+
+        if baseTr:
+            eff.Fill(sigTr,pt)
+            if (pt>180):
+                mRunTotal[evt.runNo]+=1
+                if (sigTr): mRunPass[evt.runNo]+=1
+
+    can=rt.TCanvas()
+    eff.Draw("AP")
+    print "pt>%.0f: %.1f+%.1f-%.1f"%(getPlateauEfficiency(eff,180))
+    raw_input("...")
+    can.Clear()
+    eff_run=rt.TEfficiency("effRun",";Run Number;Trigger Efficiency",len(mRunTotal),0,1)
+    i=0
+    for run in mRunTotal:
+        i+=1
+        tot=mRunTotal[run]
+        pas=mRunPass[run]
+        print run,tot,pas
+        eff_run.SetTotalEvents (i,tot)
+        eff_run.SetPassedEvents(i,pas)
+
+    hDummy=eff_run.GetPassedHistogram().Clone()
+    hDummy.Divide(eff_run.GetTotalHistogram())
+    hDummy.Draw("axis")
+    hDummy.SetMinimum(0)
+    hDummy.GetYaxis().SetNdivisions(507)
+    hDummy.GetXaxis().SetNdivisions(-1)
+    hDummy.GetXaxis().SetTitleOffset(1.5)
+    hDummy.GetXaxis().CenterLabels()
+
+    i=0
+    for iRun in mRunTotal:
+        i+=1
+        label="%d"%iRun
+        hDummy.GetXaxis().SetBinLabel(i,label);
+        print i,label
+
+    effGr=eff_run.CreateGraph()
+    effGr.Draw("p")
+
+    raw_input("...")
 
 def getPlateauEfficiency(eff, lower):
     htot=eff.GetCopyTotalHisto()
