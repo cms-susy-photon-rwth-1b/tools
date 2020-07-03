b79c8276f88ce83b27082f66d38151b34f051be5
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2020-07-03 11:28:11 +0200

    Changed displaying of jobs

--- condor_status.py
+++ condor_status.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import time
+import os
 
 def getInfos():
     out = subprocess.check_output(["condor_q", "-long"])
@@ -20,20 +21,31 @@ def getNameFromFile(fname):
     return fname.split("/")[-1].replace("_nTuple.root","").split(" ")[0]
 
 jobs = getInfos()
-jobs = sorted(jobs, key=lambda l: l["JobStatus"]+l["Args"])
+jobs = sorted(jobs, key=lambda l: l["JobStatus"]+l["ClusterId"])
+susJobs = []
 
 for job in jobs:
-    name = getNameFromFile(job["Args"])
+    if job["Cmd"].split("/")[-1]=="run.sh":
+        name = getNameFromFile(job["Args"])
+    else:
+        name = (job["Args"].split(",")[0]).split("/")[2]
     name += " "*max([0,(40-len(name))])
     jStatus = job["JobStatus"]
     if jStatus == "1":
-        print name, "idle"
+        print name, job["ClusterId"], "       idle"
     elif jStatus == "2":
-        print name, "running"
+        print name, job["ClusterId"], "       running"
+    elif jStatus == "5":
+        print name, job["ClusterId"], "       held"
     elif jStatus == "7":
         susTime = (time.time()-int(job["LastSuspensionTime"]))/60.
-        print name, "suspended since {:.2f} min".format(susTime)
+        print name, job["ClusterId"], "       suspended since {:.2f} min".format(susTime), "         "+job["RemoteHost"]
+        if susTime > 10 :
+            susJobs.append(job["ClusterId"])
     else:
         print "job status = ", jStatus
 print getSummary()
+for job in susJobs:
+    os.system("condor_hold "+job)
+if len(susJobs)!=0: os.system("condor_release dmeuser")
 
